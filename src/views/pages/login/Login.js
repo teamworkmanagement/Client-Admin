6e63ec5098a5764da6431fb8cf867d6f3c82c08c
Nguyễn Tiến Dũng <36759783+tiendunghk@users.noreply.github.com>
2021-07-22 01:01:24 +0700

    Update Login.js

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -76,25 +76,8 @@ const Login = () => {
                       <CCol xs="6">
                         <CButton color="primary" type="submit" className="px-4">Đăng nhập</CButton>
                       </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">Quên mật khẩu?</CButton>
-                      </CCol>
                     </CRow>
                   </CForm>
-
-
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Đăng ký</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Đăng ký ngay bây giờ!</CButton>
-                    </Link>
-                  </div>
                 </CCardBody>
               </CCard>
             </CCardGroup>
